6a91ad7afef49f2b6e02047cae8110e81b072426
dale-personal <51532677+dale-personal@users.noreply.github.com>
2022-03-26 18:08:16 -0500

    user agnostic paths

--- screensaver-game-select/pixelcade.sh
+++ screensaver-game-select/pixelcade.sh
@@ -3,4 +3,4 @@
 #
 # Call the game-select pixelcade script to centralize the logic
 #
-bash /home/dale/.emulationstation/scripts/game-select/pixelcade.sh "$@" > /dev/null 1> /dev/null
+bash ~/.emulationstation/scripts/game-select/pixelcade.sh "$@" > /dev/null 1> /dev/null
