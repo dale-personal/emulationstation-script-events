d108aa008469ed0a63ac883d6fe95c5242c49b0f
dale-personal <51532677+dale-personal@users.noreply.github.com>
2022-03-26 18:07:04 -0500

    user agnostic paths

--- game-select/pixelcade.sh
+++ game-select/pixelcade.sh
@@ -8,14 +8,14 @@ PIXELCADEBASEURL="http://localhost:8080/"
 PIXELCADEURL="arcade/stream/$1/$URLENCODED_FILENAME"
 
 # name of the file that stores the last marquee selected
-PREVIOUSGAMESELECTEDFILE="/home/dale/pixelcade/.game-select"
+PREVIOUSGAMESELECTEDFILE="/opt/retropie/configs/all/emulationstation/.game-select"
 
 # get the last previously selected game for the marquee
 PREVIOUSGAMESELECTED=$(cat "$PREVIOUSGAMESELECTEDFILE" 2>/dev/null)
 
 LOWER_FILENAME="$(python -c "import urllib, sys; print sys.argv[1].lower()" "${ROMNAME%.*}")"
 
-if [ ! -d "/home/dale/pixelcade/$1" ] || [ -f "/home/dale/pixelcade/$1/$LOWER_FILENAME.png" ] || [ -f "/home/dale/pixelcade/$1/$LOWER_FILENAME.gif" ]; then
+if [ ! -d "~/pixelcade/$1" ] || [ -f "~/pixelcade/$1/$LOWER_FILENAME.png" ] || [ -f "~/pixelcade/$1/$LOWER_FILENAME.gif" ]; then
   CURRENTGAMESELECTED="$1/$ROMNAME"
 else
   CURRENTGAMESELECTED="$1"
